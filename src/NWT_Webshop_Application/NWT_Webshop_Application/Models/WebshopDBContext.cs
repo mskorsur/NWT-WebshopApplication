5f2ee73828b7e3c3a8c49a22f2aa7a16e9dce017
Marin Skorsur <m.skorsur@gmail.com>
2017-02-11 19:09:03 +0100

    Angular scripts updated via webpack, database context added.

---
+++ src/NWT_Webshop_Application/NWT_Webshop_Application/Models/WebshopDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NWT_Webshop_Application.Models
+{
+    public class WebshopDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public WebshopDBContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
+
+        public static WebshopDBContext Create()
+        {
+            return new WebshopDBContext();
+        }
+    }
+}
